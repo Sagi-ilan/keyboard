e4e27cf741ffce0d08761ffdff2c02b5253c584a
Sagi Ilan <129877544+Sagi-ilan@users.noreply.github.com>
2023-04-13 09:00:55 -0400

    Add files via upload

---
+++ keyboard.go
@@ -0,0 +1,35 @@
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const DaysInWeek int = 7
+var Walllla int = 8
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+
+func GetString() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	input = strings.TrimSpace(input)
+	return input, nil
+}
